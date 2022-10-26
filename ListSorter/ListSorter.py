5a3a8e3f9ebae3ed9ef2aa63fc3d44c8b1f10aaa
Soul <61690983+IgnoredSoul@users.noreply.github.com>
2022-10-27 01:07:06 +1030

    yes

--- ListSorter/ListSorter.py
+++ ListSorter/ListSorter.py
@@ -1,14 +1,33 @@
-import os
+import os, shutil
+
+def checker(line):
+    if "VRCPlayer" in line or "_Application" in line or "UserInterface" in line or "PlayerManager" in line:
+        return True
+    return False
 
 if not(os.path.exists("./Input")):
     os.mkdir("./Input")
     input = "Created Input Folder, Press Enter To Continue..."
-if not(os.path.exists("./Output")):
-    os.mkdir("./Output")
+if not(os.path.exists("./Worlds")):
+    os.mkdir("./Worlds")
 
-for file in os.listdir("./Input"):
-    if(file.endswith(".txt")):
-        with open(f"./Input/{file}", 'r') as o:
-            with open(f'./Output/{file[:-4]}.txt', 'w') as s:
-                for line in sorted(o):
-                    s.write(line)
+# for file in os.listdir("./Input"):
+#     if(file.endswith(".txt")):
+#         with open(f"./Input/{file}", 'r') as o:
+#             with open(f'./Output/{file[:-4]}.txt', 'w') as s:
+#                 for line in sorted(o):
+#                     if not checker(line):
+#                         s.write(line)
+
+for file in os.listdir("./Output"):
+    if "GameObjectList" in file:
+        name = file.split("GameObjectList")[0][:-3]
+        if not(os.path.exists(f"./Worlds/{name}")): os.mkdir(f"./Worlds/{name}")
+        shutil.move(f"./Output/{file}", f"./Worlds/{name}")
+    if "UdonKeyList" in file:
+        name = file.split("UdonKeyList")[0][:-3]
+        if not(os.path.exists(f"./Worlds/{name}")): os.mkdir(f"./Worlds/{name}")
+        shutil.move(f"./Output/{file}", f"./Worlds/{name}")
+
+print("Finished")
+
--- VRCQM-Console/QuickModTester.py
+++ VRCQM-Console/QuickModTester.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     try:
         os.system("cls")
         os.system("adb.exe kill-server")
-        os.system("adb.exe push (path/.dll) /sdcard/Android/data/com.vrchat.oculus.quest/files/Mods");
+        os.system("adb.exe push C:/Users/Jakei/source/repos/GRIM/GRIM/bin/Debug/GRIM.dll /sdcard/Android/data/com.vrchat.oculus.quest/files/Mods");
         os.system("adb shell am force-stop com.vrchat.oculus.quest")
         time.sleep(2)
         os.system("adb.exe shell monkey -p com.vrchat.oculus.quest -v 1")
